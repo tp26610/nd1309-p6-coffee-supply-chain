b75fc560029a81c85a042135acab6b9df14426ae
Brian Yeh <brian.yeh@coolbitx.com>
2021-08-22 22:10:11 +0800

    test: test FarmerAdded and FarmerRemoved event emitted

--- project-6/test/coffeeaccesscontrol/TestFarmerRole.js
+++ project-6/test/coffeeaccesscontrol/TestFarmerRole.js
@@ -1,18 +1,22 @@
+// import EventTester from '../EventTester';
+const EventTester = require('../EventTester');
 const FarmerRole = artifacts.require('FarmerRole');
 
 contract('FarmerRole', (accounts) => {
 
+  let farmerRole;
   const ownerAddress = accounts[0];
   const farmer1 = accounts[1];
 
+  beforeEach(async () => {
+    farmerRole = await FarmerRole.new();
+  });
+
   it('is farmer for addresses added', async () => {
-    const farmerRole = await FarmerRole.deployed();
     assert.equal(await farmerRole.isFarmer(ownerAddress), true)
   });
 
   it('adds a farmer', async () => {
-    const farmerRole = await FarmerRole.deployed();
-
     // farmer1 is not a farmer
     assert.equal(await farmerRole.isFarmer(farmer1), false);
 
@@ -21,9 +25,33 @@ contract('FarmerRole', (accounts) => {
     assert.equal(await farmerRole.isFarmer(farmer1), true);
   });
 
+  it('emits FarmerAdded event', async () => {
+    // farmer1 is not a farmer
+    assert.equal(await farmerRole.isFarmer(farmer1), false);
+
+    // listen event FarmerAdded
+    const eventTester = new EventTester();
+    await eventTester.watchEvent(farmerRole.FarmerAdded());
+
+    await farmerRole.addFarmer(farmer1);
+
+    // assert FarmerAdded event emitted
+    await eventTester.assertEvent('FarmerAdded', { account: farmer1 });
+  });
+
   it('renounces a farmer', async () => {
-    const farmerRole = await FarmerRole.deployed();
     await farmerRole.renounceFarmer();
     assert.equal(await farmerRole.isFarmer(ownerAddress), false);
+  });
+
+  it('emits FarmerRemoved event', async () => {
+    // listen event FarmerAdded
+    const eventTester = new EventTester();
+    await eventTester.watchEvent(farmerRole.FarmerRemoved());
+
+    await farmerRole.renounceFarmer();
+
+    // assert FarmerAdded event emitted
+    await eventTester.assertEvent('FarmerRemoved', { account: ownerAddress });
   });
 });
