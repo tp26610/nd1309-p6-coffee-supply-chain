243b60b7169a675c13c40fba5062aec0e16f6854
Brian Yeh <brian.yeh@coolbitx.com>
2021-09-04 19:26:45 +0800

    refactor: truffle config

--- project-6/truffle.js
+++ project-6/truffle.js
@@ -1,8 +1,6 @@
 const HDWalletProvider = require("@truffle/hdwallet-provider");
 const infuraKey = "b8b1463eca1b41319a542b51d12428de";
-//
 const fs = require('fs');
-const mnemonic = fs.readFileSync(".secret").toString().trim();
 
 module.exports = {
   networks: {
@@ -12,10 +10,13 @@ module.exports = {
       network_id: "*" // Match any network id
     },
     rinkeby: {
-      provider: () => new HDWalletProvider(mnemonic, `https://rinkeby.infura.io/v3/${infuraKey}`),
-        network_id: 4,       // rinkeby's id
-        gas: 4500000,        // rinkeby has a lower block limit than mainnet
-        gasPrice: 10000000000
+      provider: () => {
+        const mnemonic = fs.readFileSync(".secret").toString().trim();
+        return new HDWalletProvider(mnemonic, `https://rinkeby.infura.io/v3/${infuraKey}`)
+      },
+      network_id: 4,       // rinkeby's id
+      gas: 4500000,        // rinkeby has a lower block limit than mainnet
+      gasPrice: 10000000000
     },
   }
 };
